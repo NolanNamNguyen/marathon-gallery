eceba56ea778fe19feb3f66eb82a6b7cdf9a253a
Nam Nguyen Van <namvannguyen.andy@gmail.com>
2023-01-20 00:45:36 +0700

    ga gay te le te le sang roi ai oi

--- redux/slices/homeSlice.js
+++ redux/slices/homeSlice.js
@@ -58,11 +58,12 @@ export const homeSlice = createSlice({
     },
     getImagesSuccess: (state, { payload: { images } }) => {
       if (images.length) {
-        // const results = images.map((i) => ({
-        //   ...i,
-        //   thumbnail: `${process.env.NEXT_PUBLIC_API_URL}/${i.thumbnail}`,
-        // }));
-        state.images = images;
+        const results = images.map((i) => ({
+          ...i,
+          thumbnail: `${process.env.NEXT_PUBLIC_API_URL}/${i.thumbnail}`,
+          full_size: `${process.env.NEXT_PUBLIC_API_URL}/${i.full_size}`,
+        }));
+        state.images = results;
       }
       state.fetchingImages = false;
     },
